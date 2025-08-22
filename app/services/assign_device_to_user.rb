c0e768e57cc69b0e4b6bec7f41b48a0813917787
witoldszgl <witold.szumigala@gmail.com>
2025-08-22 20:52:23 +0200

    Revert "FEAT: implement device assignment services"
    This reverts commit bafb472d6a694837c475e227d9c71fa2da21ef5c.

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -1,24 +1,9 @@
 # frozen_string_literal: true
 
 class AssignDeviceToUser
-  attr_reader :user, :serial_number
-
-  def initialize(user:, serial_number:)
-    @user = user
-    @serial_number = serial_number
+  def initialize(requesting_user:, serial_number:, new_device_owner_id:)
   end
 
   def call
-    device = Device.find_or_create_by(serial_number: serial_number)
-    
-    if device.user_id.present?
-      raise StandardError, "Device is already assigned"
-    end
-
-    if device.returned_by_user?(user.id)
-      raise StandardError, "User has previously returned this device and cannot reassign it"
-    end
-
-    device.assign(user)
   end
 end
--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -1,25 +1,11 @@
 # frozen_string_literal: true
 
 class ReturnDeviceFromUser
-  attr_reader :user, :serial_number
-
-  def initialize(user:, serial_number:)
-    @user = user
-    @serial_number = serial_number
+  def initialize
+    # TODO
   end
 
   def call
-    device = Device.find_by(serial_number: serial_number)
-    raise StandardError, "Device not found" unless device
-
-    unless device.user_id.present?
-      raise StandardError, "Device is not currently assigned"
-    end
-
-    unless device.user == user
-      raise StandardError, "Only the user who assigned the device can return it"
-    end
-
-    device.unassign(user)
+    # TODO
   end
 end
