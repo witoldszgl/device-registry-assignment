bafb472d6a694837c475e227d9c71fa2da21ef5c
Witold Szumigała <witold.szumigala@gmail.com>
2025-08-22 16:04:10 +0200

    FEAT: implement device assignment services

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
 class ReturnDeviceFromUser
-  def initialize
-    # TODO
+  attr_reader :user, :serial_number
+
+  def initialize(user:, serial_number:)
+    @user = user
+    @serial_number = serial_number
   end
 
   def call
-    # TODO
+    device = Device.find_by(serial_number: serial_number)
+    raise StandardError, "Device not found" unless device
+
+    unless device.user_id.present?
+      raise StandardError, "Device is not currently assigned"
+    end
+
+    unless device.user == user
+      raise StandardError, "Only the user who assigned the device can return it"
+    end
+
+    device.unassign(user)
   end
 end
